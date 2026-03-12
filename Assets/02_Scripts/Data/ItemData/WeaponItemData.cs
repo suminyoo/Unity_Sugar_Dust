5e50ab215688e6bc92f9a13d0d3497cf4189eea8
suminyoo <60617409+suminyoo@users.noreply.github.com>
2026-03-12 19:52:51 +0900

    Add sword and pickaxe table data and item prefabs

--- Assets/02_Scripts/Data/ItemData/WeaponItemData.cs
+++ Assets/02_Scripts/Data/ItemData/WeaponItemData.cs
@@ -1,4 +1,14 @@
 using UnityEngine;
+public enum ToolType
+{
+    None,
+    Blue,
+    Green,
+    Red,
+    White,
+    Black,
+    Rainbow  
+}
 
 [CreateAssetMenu(fileName = "New Weapon Item", menuName = "Game/Item Data/Weapon")]
 public class WeaponItemData : ItemData
@@ -7,4 +17,9 @@ public class WeaponItemData : ItemData
     public float attackDamage;
     public float attackSpeed;
     public float cooldown;
+
+    public Vector3 attackBoxSize = new Vector3(1f, 1f, 1f); 
+
+    public ToolType toolType = ToolType.None;
+
 }
